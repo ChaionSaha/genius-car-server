9ec856fe30c7488c2220f04f1e5e9632b16d4ac2
Chaion Saha <sahachaion2000@gmail.com>
2023-03-14 20:31:44 +0600

    single service query added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,11 +15,30 @@ const client = new MongoClient(uri, {
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-	const collection = client.db('geniusCar').collection('services');
-	console.log('DB connected');
-	client.close();
-});
+
+async function run() {
+	try {
+		await client.connect();
+		const serviceCollection = client.db('geniusCar').collection('services');
+
+		app.get('/services', async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.toArray();
+			res.send(services);
+		});
+
+		app.get('/service/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const services = await serviceCollection.findOne(query);
+			res.send(services);
+		});
+	} finally {
+	}
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('Server is running');
