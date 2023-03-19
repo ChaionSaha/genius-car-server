177b21f5bdfec76fde3ead1129fc7610e504e2db
Chaion Saha <chaionsaha00@gmail.com>
2023-03-19 21:53:08 +0600

    applied checkout page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,33 +19,42 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		await client.connect();
-		const serviceCollection = client.db('geniusCar').collection('services');
+		const serviceCollection = client.db("geniusCar").collection("services");
+		const orderCollection = client.db("geniusCar").collection("orders");
 
-		app.get('/services', async (req, res) => {
+		app.get("/services", async (req, res) => {
 			const query = {};
 			const cursor = serviceCollection.find(query);
 			const services = await cursor.toArray();
 			res.send(services);
 		});
 
-		app.get('/service/:id', async (req, res) => {
+		app.get("/service/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const services = await serviceCollection.findOne(query);
 			res.send(services);
 		});
 
-		app.post('/service', async (req, res) => {
+		app.post("/service", async (req, res) => {
 			const service = req.body;
 			const result = await serviceCollection.insertOne(service);
 
 			res.send(result);
 		});
 
-		app.delete('/service/:id', async (req, res) => {
+		app.delete("/service/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await serviceCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		app.post("/order", async (req, res) => {
+			const order = req.body;
+			const result = await orderCollection.insertOne(order);
+
+			console.log(result);
 			res.send(result);
 		});
 	} finally {
@@ -54,8 +63,8 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-	res.send('Server is running');
+app.get("/", (req, res) => {
+	res.send("Server is running");
 });
 
 app.listen(port, () => {
