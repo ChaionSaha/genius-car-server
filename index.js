ea802580e47a1184226cd52c150df73e54f25bea
Chaion Saha <chaionsaha00@gmail.com>
2023-03-20 23:30:39 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -16,12 +17,40 @@ const client = new MongoClient(uri, {
 	serverApi: ServerApiVersion.v1,
 });
 
+const verifyJWT = async (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).send({ message: 'unauthorized access' });
+	}
+	const token = authHeader.split(' ')[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) return res.status(403).send({ message: 'forbidden' });
+		req.decoded = decoded;
+		next();
+	});
+};
+
 async function run() {
 	try {
 		await client.connect();
 		const serviceCollection = client.db('geniusCar').collection('services');
 		const orderCollection = client.db('geniusCar').collection('orders');
 
+		app.post('/login', async (req, res) => {
+			const user = req.body;
+			const accessToken = await jwt.sign(
+				user,
+				process.env.ACCESS_TOKEN_SECRET,
+				{
+					expiresIn: '1d',
+				}
+			);
+
+			res.send(accessToken);
+		});
+
+		////////////////////////////////////
+		// Services Section
 		app.get('/services', async (req, res) => {
 			const query = {};
 			const cursor = serviceCollection.find(query);
@@ -59,12 +88,14 @@ async function run() {
 			res.send(result);
 		});
 
-		app.get('/orders', async (req, res) => {
+		app.get('/orders', verifyJWT, async (req, res) => {
+			const decodedEmail = req.decoded.email;
 			const email = req.query.email;
+			if (email !== decodedEmail)
+				return res.status(403).send({ message: 'forbidden access' });
 			const query = { email: email };
 			const cursor = orderCollection.find(query);
 			const orders = await cursor.toArray();
-
 			res.send(orders);
 		});
 	} finally {
